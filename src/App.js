c47def62d99f434f6568ef494d0117bf853672f3
envincebal <vincentyan1@gmail.com>
2018-03-21 02:50:11 -0700

    Added title and details handlers

--- src/App.js
+++ src/App.js
@@ -6,17 +6,36 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			notes: []
+			notes: [],
+			title: "",
+			details: ""
 		}
+		this.updateTitle = this.updateTitle.bind(this);
+		this.updateDetails = this.updateDetails.bind(this);
+
+	}
+
+	updateTitle(event){
+		this.setState({title: event.target.value});
+	}
+
+	updateDetails(event){
+		this.setState({details: event.target.value});
 	}
 
 	render() {
 		return (
 			<div className="container">
 				<h1 className="title">React Notes App</h1>
-				<NoteForm />
+				<NoteForm
+					titleValue={this.state.title}
+					detailsValue={this.state.details}
+					titleHandle={this.updateTitle}
+					detailsHandle={this.updateDetails}
+					prevent={(e) => e.preventDefault()}
+				/>
 				<div className="entry-section">
-
+					{this.state.notes.map(() => <NoteEntry/> )}
 				</div>
 			</div>
 		);
--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -4,9 +4,28 @@ const NoteForm = (props) => {
   return (
     <div>
       <form className="form-section">
-        <input className="title-input" type="text" autoFocus placeholder="Title" /><br/>
-        <textarea className="details-input" cols="20" rows="3" placeholder="Details" ></textarea><br/>
-        <button className="input-button">Add Note</button>
+        <input
+          className="title-input"
+          type="type"
+          placeholder="Title"
+          onSubmit={props.prevent}
+          value={props.titleValue}
+          onChange={props.titleHandle}
+        />
+        <br />
+        <textarea
+          className="details-input"
+          cols="20"
+          rows="3"
+          placeholder="Details"
+          value={props.detailsValue}
+          onChange={props.detailsHandle}
+          />
+        <br />
+        <button
+          className="input-button"
+          onClick={props.prevent}
+        >Add Note</button>
       </form>
     </div>
   )
